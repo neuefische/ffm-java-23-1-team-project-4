a0b703bde2e763e6c0a558fd549a1d3cf0178a8e
Rylie Castell <rylie.castell@gmail.com>
2023-10-09 12:13:37 +0200

    add styling

--- frontend/src/EntryComponent.tsx
+++ frontend/src/EntryComponent.tsx
@@ -1,9 +1,53 @@
 import {BlogEntry} from "./model/BlogEntryModel.tsx";
+import styled from "styled-components";
 
 export type props = {
     blogEntry: BlogEntry,
     onDeleteEntry: (id: string) => void;
 }
+
+const Container = styled.li`
+  border: 1px black solid;
+  border-radius: 10px;
+  background-color: #646cff;
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  padding: 0.6em;
+  width: 100%;
+  position: relative;
+  left: 0;
+  right: 0;
+  align-self: center;
+`;
+
+const Title = styled.h2`
+  font-size: 2em;`;
+
+const TitleContainer = styled.div`
+  display: flex;
+  justify-content: center;`
+
+const BookmarkButton = styled.button`
+  width: 1em;
+  border-radius: 10px;
+  border: 0.2em black solid;
+  position: absolute;
+  top: 0;
+  right: 0;
+`;
+
+const TagList = styled.ul`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+`;
+
+const Tag = styled.li`
+border: solid black 1px;
+padding: 0.2em;`;
+
+
 export default function EntryComponent(props: props) {
 
     function handleClickBookmark() {
@@ -11,16 +55,18 @@ export default function EntryComponent(props: props) {
     }
 
     return <>
-        <li>
-            <h2>{props.blogEntry.title}</h2>
-            <small>01.01.2000</small>
-            <button type="button" onClick={handleClickBookmark}>Bookmark</button>
+        <Container>
+            <TitleContainer>
+                <Title>{props.blogEntry.title}</Title>
+                <small>01.01.2000</small>
+            </TitleContainer>
+            <BookmarkButton type="button" onClick={handleClickBookmark}>Bookmark</BookmarkButton>
             <p>{props.blogEntry.content}</p>
             <button type="button" onClick={() => props.onDeleteEntry(props.blogEntry.id)}>Delete</button>
-            <ul>Tags:
-                <li>#Tag1</li>
-                <li>#Tag2</li>
-            </ul>
-        </li>
+            <TagList>Tags:
+                <Tag>#Tag1</Tag>
+                <Tag>#Tag2</Tag>
+            </TagList>
+        </Container>
     </>
 }
