b064adfc2de148fe97f98dd67e1d4590d06ac376
Rylie Castell <rylie.castell@gmail.com>
2023-10-11 15:45:08 +0200

    add styling to new entry page

--- frontend/src/pages/blogentry/NewBlogEntry.tsx
+++ frontend/src/pages/blogentry/NewBlogEntry.tsx
@@ -2,18 +2,92 @@ import {useState} from "react";
 import axios from "axios";
 import {useNavigate} from "react-router-dom";
 import {NewBlog, Tag} from "./model/model.ts";
+import styled from "styled-components";
+import AppHeader from "../../components/AppHeader.tsx";
+import AddSvg from "../../assets/plus-circle.svg"
 
-export default function NewBlogEntry(){
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 0.6em;
+  gap: 0.6em;
+`;
 
-    const [title, setTitle, ] = useState("")
-    const [text, setText, ] = useState("")
-    const [tags, setTags,] = useState<Tag[]>([{name : ""}])
+const TitleInput = styled.input`
+  font-size: 1.4em;
+`;
+
+const ContentTextarea = styled.textarea`
+  font-size: 1.2em;
+`;
+
+const TagsTitle = styled.span`
+  font-size: 1.2em;
+`;
+
+const TagContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.4em;
+`;
+
+const SingleTag = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-content: center;
+  gap: 0.4em;
+`;
+
+const TagLabel = styled.label`
+  font-size: 1.2em;
+  width: 2em;
+  text-align: end;
+  align-self: center;
+`;
+
+const TagInput = styled.input`
+  font-size: 1.2em;
+`;
+
+const TagButton = styled.button`
+  width: 3em;
+  height: 3em;
+  font-size: 1em;
+  position: relative;
+  background: none;
+  border: none;
+`;
+
+const ButtonImage = styled.img`
+width: 2.4em;
+  position: absolute;
+  top: 0.2em;
+  left: 0;
+`;
+
+const SubmitButton = styled.button`
+  border-radius: 10px;
+  padding: 0.6em;
+  background-color: #90d2d8;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  font-size: 1.6em;
+`;
+
+export default function NewBlogEntry() {
+
+    const [title, setTitle,] = useState("")
+    const [text, setText,] = useState("")
+    const [tags, setTags,] = useState<Tag[]>([{name: ""}])
 
     const addNewTag = () => {
         setTags([...tags, {name: ""}]);
     };
 
-    const changeTagName = (index: number, name : string) => {
+    const changeTagName = (index: number, name: string) => {
         const newTags = [...tags]; // Clone the tags array
         newTags[index].name = name; // Change the name of the tag at the given index
         setTags(newTags); // Update the state
@@ -21,26 +95,25 @@ export default function NewBlogEntry(){
 
     const navigateTo = useNavigate()
 
-    function onSubmit()
-    {
+    function handleOnSubmit() {
         //function to delete and entry with the name default text
         const filteredTags = tags.filter(tag => tag.name !== "");
-        const mytags = filteredTags.map((item) => item.name )
+        const mytags = filteredTags.map((item) => item.name)
 
         const newBlogEntry: NewBlog =
-        {
-            title: title,
-            content: text,
-            hashtags : mytags,
-        };
+            {
+                title: title,
+                content: text,
+                hashtags: mytags,
+            };
         axios
-            .post("/api/blogs",newBlogEntry)
+            .post("/api/blogs", newBlogEntry)
             .then((response) => {
 
                 console.log("Erfolgreich gespeichert:", response.data);
                 // Füge hier ggf. weitere Aktionen nach erfolgreicher Speicherung hinzu
             })
-            .then(()=>navigateTo("/"))
+            .then(() => navigateTo("/"))
             .catch((error) => {
                 // Fehler verarbeiten
                 console.error("Fehler beim Speichern:", error);
@@ -50,30 +123,32 @@ export default function NewBlogEntry(){
     }
 
 
-return  <>
-    <h1>NewBlog</h1>
-        <p>
-            <input placeholder={"title"} value={title} onChange={(event)=>setTitle(event.target.value)}></input>
-        </p>
-
-        <p>
-            <textarea rows={10}  placeholder={"write your text here"} value={text} onChange={(event)=>setText(event.target.value)}></textarea>
-        </p>
-
-        <div>
-            <span>Tags:</span>
-            {tags && tags.map((tag, index) => (
-                <div key={index}>
-                    <span>{index + 1}. </span>
-                    <input
-                        value={tag.name}
-                        placeholder={"#Enter a Tag"}
-                        onChange={(event) => changeTagName(index, event.target.value)}
-                    />
-                    <button onClick={() => addNewTag()}> + </button>
-                </div>
-            ))}
-        </div>
-        <button onClick={onSubmit}>Submit</button>
-</>
+    return <>
+        <AppHeader headerText="New Blog"/>
+        <Main>
+            <TitleInput type="text" placeholder="What I love to eat" value={title}
+                        onChange={(event) => setTitle(event.target.value)}></TitleInput>
+            <ContentTextarea rows={10} placeholder={"Lorem ipsum dolor sit amet, consetetur sadipscing elitr."}
+                             value={text}
+                             onChange={(event) => setText(event.target.value)}></ContentTextarea>
+            <TagsTitle>Tags:</TagsTitle>
+            <TagContainer>
+                {tags && tags.map((tag, index) => (
+                    <SingleTag key={index}>
+                        <TagLabel htmlFor={"tag" + (index + 1)}>{index + 1}. </TagLabel>
+                        <TagInput
+                            id={"tag" + (index + 1)}
+                            value={tag.name}
+                            placeholder={"#HashTag"}
+                            onChange={(event) => changeTagName(index, event.target.value)}
+                        />
+                        <TagButton type="button" onClick={() => addNewTag()}>
+                            <ButtonImage src={AddSvg} alt="Add Icon"/>
+                        </TagButton>
+                    </SingleTag>
+                ))}
+            </TagContainer>
+            <SubmitButton onClick={handleOnSubmit}>Submit</SubmitButton>
+        </Main>
+    </>
 }
