9fbd58dedaf149a23ce74f681f86cf8bf799aa48
Rylie Castell <rylie.castell@gmail.com>
2023-10-11 16:12:52 +0200

    add styling to edit entry page

--- frontend/src/pages/blogentry/EditBlogEntry.tsx
+++ frontend/src/pages/blogentry/EditBlogEntry.tsx
@@ -2,21 +2,100 @@ import {BlogEntry} from "../../model/BlogEntryModel.tsx";
 import {useNavigate, useParams} from "react-router-dom";
 import axios from "axios";
 import React, {useEffect, useState} from "react";
+import AppHeader from "../../components/AppHeader.tsx";
+import styled from "styled-components";
+import AddSvg from "../../assets/plus-circle.svg";
+import MinusSvg from "../../assets/minus-circle.svg";
+
+const EditForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 0.6em;
+  gap: 0.6em;
+`;
+
+const TitleInput = styled.input`
+  font-size: 1.4em;
+`;
+
+const ContentTextarea = styled.textarea`
+  font-size: 1.2em;
+`;
+
+const TagsTitle = styled.span`
+  font-size: 1.2em;
+`;
+
+const TagContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.4em;
+`;
+
+const SingleTag = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-content: center;
+  gap: 0.4em;
+`;
+
+const TagLabel = styled.label`
+  font-size: 1.2em;
+  width: 2em;
+  text-align: end;
+  align-self: center;
+`;
+
+const TagInput = styled.input`
+  font-size: 1.2em;
+`;
+
+const TagButton = styled.button`
+  width: 3em;
+  height: 3em;
+  font-size: 1em;
+  position: relative;
+  background: none;
+  border: none;
+`;
+
+const ButtonImage = styled.img`
+  width: 2.4em;
+  position: absolute;
+  top: 0.2em;
+  left: 0;
+`;
+
+const ButtonContainer = styled.div`
+    display: flex;
+  gap: 0.6em;
+  justify-content: stretch;
+`;
+
+const Button = styled.button`
+  border-radius: 10px;
+  padding: 0.6em;
+  background-color: #90d2d8;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  font-size: 1.2em;
+`;
 
 
-export default function EditBlogEntry(){
+export default function EditBlogEntry() {
 
     const navigateTo = useNavigate()
-    const { id } = useParams();
-    const [blogentry , setBlogentry] = useState<BlogEntry>()
+    const {id} = useParams();
+    const [blogentry, setBlogentry] = useState<BlogEntry>()
     const [loading, setLoading] = useState<boolean>(true);
-    const [error , setError] = useState<boolean>(false);
+    const [error, setError] = useState<boolean>(false);
 
 
-    useEffect( () =>
-    {
-        const fetchBlog = async () =>
-        {
+    useEffect(() => {
+        const fetchBlog = async () => {
             try {
                 const response = await axios(`/api/blogs/${id}`);
                 setBlogentry(response.data);
@@ -30,8 +109,8 @@ export default function EditBlogEntry(){
     }, [id]);
 
     const addNewTag = () => {
-        if(blogentry)
-            setBlogentry({ ...blogentry, hashtags : [...blogentry.hashtags, ""] });
+        if (blogentry)
+            setBlogentry({...blogentry, hashtags: [...blogentry.hashtags, ""]});
     };
 
 
@@ -39,7 +118,7 @@ export default function EditBlogEntry(){
         if (blogentry && blogentry.hashtags) {
             const newTags = [...blogentry.hashtags];
             newTags.splice(index, 1);
-            setBlogentry({ ...blogentry, hashtags: newTags });
+            setBlogentry({...blogentry, hashtags: newTags});
         }
     };
 
@@ -54,13 +133,13 @@ export default function EditBlogEntry(){
             const formattedDate = date.toISOString()
 
             const changedBlogEntry: BlogEntry =
-            {
-                id: blogentry.id,
-                title: blogentry.title,
-                content: blogentry.content,
-                hashtags: mytags,
-                timeCreated: formattedDate,
-            };
+                {
+                    id: blogentry.id,
+                    title: blogentry.title,
+                    content: blogentry.content,
+                    hashtags: mytags,
+                    timeCreated: formattedDate,
+                };
             axios
                 .put("/api/blogs/" + id, changedBlogEntry)
                 .then(() => navigateTo("/"))
@@ -70,54 +149,58 @@ export default function EditBlogEntry(){
         }
     }
 
-    if(loading) return <div>Loading...</div>
-    if(error) return <div>Something went wrong</div>
+    if (loading) return <div>Loading...</div>
+    if (error) return <div>Something went wrong</div>
 
-return(
-<>
-<form onSubmit={handleSubmit}>
-    <h1>NewBlog</h1>
+    return (
+        <>
+            <AppHeader headerText={"Edit Blog"}/>
+            <EditForm onSubmit={handleSubmit}>
+                {blogentry && (
+                    <>
+                        <TitleInput
+                            type="text"
+                            value={blogentry.title || ''}
+                            onChange={(e) => {
+                                setBlogentry({...blogentry, title: e.target.value});
+                            }}/>
+                        <ContentTextarea rows={23}
+                                         value={blogentry.content}
+                                         onChange={(e) => {
+                                             setBlogentry({...blogentry, content: e.target.value});
+                                         }}/>
+                        <TagsTitle>Tags:</TagsTitle>
+                        <TagContainer>
+                        {blogentry.hashtags && blogentry.hashtags.map((tag, index) => (
+                            <SingleTag key={index}>
+                                <TagLabel htmlFor={"tag" + (index + 1)}>{index + 1}. </TagLabel>
+                                <TagInput
+                                    maxLength={20}
+                                    type="text"
+                                    id={"tag" + (index + 1)}
+                                    value={tag}
+                                    onChange={((event) => {
+                                        const newTags = [...blogentry.hashtags];
+                                        newTags[index] = event.target.value;
+                                        setBlogentry({...blogentry, hashtags: newTags});
+                                    })}
+                                />
 
-
-    {blogentry && (
-    <div>
-        <p>
-            <input
-                value={blogentry.title || ''}
-                onChange={(e) => {
-                    setBlogentry({...blogentry, title: e.target.value});
-                }}/>
-        </p>
-
-        <p>
-            <textarea rows={10}
-                value={blogentry.content}
-                onChange={(e) => {
-                   setBlogentry({...blogentry, content: e.target.value});
-                }}/>
-        </p>
-        <span>Tags:</span>
-        {blogentry.hashtags && blogentry.hashtags.map((tag, index) => (
-            <div key={index}>
-                <span>{index + 1}. </span>
-                <input
-                    value={tag}
-                    onChange={((event ) => {
-                        const newTags = [...blogentry.hashtags];
-                        newTags[index] = event.target.value;
-                        setBlogentry({ ...blogentry, hashtags: newTags });
-                    })}
-                />
-
-                <button type={"button"} onClick={addNewTag}> + </button>
-                <button type={"button"} onClick={() => deleteTag(index)}> - </button>
-            </div>
-        ))}
-    </div>)}
-
-    <button onClick={() => navigateTo("/")}>Discard Changes</button>
-    <button onClick={handleSubmit}> Save Changes</button>
-</form>
-</>
- )
+                                <TagButton type={"button"} onClick={addNewTag}>
+                                    <ButtonImage src={AddSvg} alt="Add Icon"/>
+                                </TagButton>
+                                <TagButton type={"button"} onClick={() => deleteTag(index)}>
+                                    <ButtonImage src={MinusSvg} alt="Reduce Icon"/>
+                                </TagButton>
+                            </SingleTag>
+                        ))}
+                    </TagContainer>
+                    </>)}
+<ButtonContainer>
+                <Button type="button" onClick={() => navigateTo("/")}>Discard</Button>
+                <Button type="submit" onClick={handleSubmit}> Save</Button>
+            </ButtonContainer>
+            </EditForm>
+        </>
+    )
 }
